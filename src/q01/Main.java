adbaf917e649eb76d75704e5bb298caf09c62f53
vermaprakhar43-wq <vermaprakhar43@gmail.com>
2026-04-08 13:33:55 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -15,7 +15,7 @@ public class Main {
            
             System.out.print("*");
 
-            
+        
             if (i > 1) {
                 for (int j = 1; j <= 2 * i - 3; j++) {
                     System.out.print(" ");
