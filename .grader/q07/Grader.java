202735ae7e2e977b04437b7e049306addee56648
vermaprakhar43-wq <vermaprakhar43@gmail.com>
2026-04-08 13:26:01 +0530

    Update Grader.java

--- .grader/q07/Grader.java
+++ .grader/q07/Grader.java
@@ -1,10 +1,10 @@
 import java.io.*;
 public class Grader {
     static final String[][] T = {
-        {"5\n64 34 25 12 22","12 22 25 34 64\nSwaps: 7"},
+        {"5\n64 34 25 12 22","12 22 25 34 64\nSwaps: 9"},
         {"4\n4 3 2 1","1 2 3 4\nSwaps: 6"},
         {"3\n1 2 3","1 2 3\nSwaps: 0"},
-        {"5\n5 1 4 2 8","1 2 4 5 8\nSwaps: 5"},
+        {"5\n5 1 4 2 8","1 2 4 5 8\nSwaps: 4"},
     };
     static String run(String in) throws Exception {
         ProcessBuilder pb=new ProcessBuilder("java","-cp","src/q07","Main");
