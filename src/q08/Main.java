cba96e894feac62b5ecfd766da36244fd4f6c657
vermaprakhar43-wq <vermaprakhar43@gmail.com>
2026-04-08 13:31:08 +0530

    Update Main.java

--- src/q08/Main.java
+++ src/q08/Main.java
@@ -4,28 +4,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.nextLine(); 
 
-        // TODO: Implement a stack using an array (no Java Stack class)
-        //       Process n operations:
-        //         PUSH x  → push integer x
-        //         POP     → pop top, print it; if empty print "EMPTY"
-        //         PEEK    → print top without removing; if empty print "EMPTY"
-        //         SIZE    → print current number of elements
-        //
-        // Input:
-        // 6
-        // PUSH 10
-        // PUSH 20
-        // PEEK
-        // POP
-        // POP
-        // POP
-        //
-        // Output:
-        // 20
-        // 20
-        // 10
-        // EMPTY
+        int[] stack = new int[n]; 
+        int top = -1;             
 
+        for (int i = 0; i < n; i++) {
+            String command = sc.nextLine();
+
+            if (command.startsWith("PUSH")) {
+                int x = Integer.parseInt(command.split(" ")[1]);
+                top++;
+                stack[top] = x;
+            } else if (command.equals("POP")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top]);
+                    top--;
+                }
+            } else if (command.equals("PEEK")) {
+                if (top == -1) {
+                    System.out.println("EMPTY");
+                } else {
+                    System.out.println(stack[top]);
+                }
+            } else if (command.equals("SIZE")) {
+                System.out.println(top + 1);
+            }
+        }
+
+        sc.close();
     }
 }
