776650109512a8bcb932d891457a1d9bfc5ade4a
vermaprakhar43-wq <vermaprakhar43@gmail.com>
2026-04-08 13:24:03 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -5,21 +5,46 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
-        // Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
+       
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
 
+           
+            System.out.print("*");
+
+            
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+      
+        for (int i = n - 1; i >= 1; i--) {
+           
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+          
+            System.out.print("*");
+
+            
+            if (i > 1) {
+                for (int j = 1; j <= 2 * i - 3; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
     }
 }
