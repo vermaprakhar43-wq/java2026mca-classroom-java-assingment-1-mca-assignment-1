bfc35b1b85b0d6017edd90082885871beafb8065
vermaprakhar43-wq <vermaprakhar43@gmail.com>
2026-04-08 13:29:10 +0530

    Update Main.java

--- src/q06/Main.java
+++ src/q06/Main.java
@@ -5,11 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // TODO: Print first n Fibonacci numbers separated by spaces
-        //       F(0)=0, F(1)=1, F(n)=F(n-1)+F(n-2)
-        //
-        // Input: 8
-        // Output: 0 1 1 2 3 5 8 13
+        int a = 0, b = 1;
 
+        for (int i = 0; i < n; i++) {
+            System.out.print(a);
+            if (i != n - 1) {
+                System.out.print(" ");
+            }
+            int next = a + b;
+            a = b;
+            b = next;
+        }
     }
 }
